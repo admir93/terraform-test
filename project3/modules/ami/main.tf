d52600f216b0bc53fb228c0a2df81de7dfa895ad
admir93 <45433544+admir93@users.noreply.github.com>
2019-03-14 10:12:21 +0100

    Add files via upload

--- project3/modules/ami/main.tf
+++ project3/modules/ami/main.tf
@@ -1,17 +1,9 @@
-resource "random_string" "default" {
-  length = 5
-  upper = false
-  lower = true
-  number = true
-  special = false
-}
-
 resource "aws_ami_from_instance" "web_app_ami" {
-  name  = "ami-${var.environment}-${var.project}-${random_string.default.result}"
+  name  = "ami-${var.environment}-${var.project}-${var.new_ami}"
   source_instance_id = "${var.webapp_ec2_id}"
   snapshot_without_reboot = true
 
   tags = {
-      Name = "ami-${var.environment}-${var.project}-${timestamp()}"
+      Name = "ami-${var.environment}-${var.project}-${var.new_ami}"
   }
 }
